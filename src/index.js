e23764cf856c043b8f1e7fe0cc718a459b22dfa7
Krishnakant010 <krishnakantgangurde0109@gmail.com>
2023-09-10 19:23:18 +0530

    sequelize setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
 const setupAndStartServer = async () => {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   const PORT = process.env.PORT;
   app.listen(PORT, () => {
     console.log("object listening on port", PORT);
